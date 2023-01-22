e275c2faddd6c1b00c88c811e3fcc2eb277cb691
isaac0310 <isaacmoton@yahoo.com>
2023-01-23 00:52:54 +0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,12 @@
+// Initialize Firebase
+var firebaseConfig = {
+    apiKey: "AIzaSyBUNAGBZ6IXbopLDfpqEQxdBsE2dA_N9j8",
+    authDomain: "halinhub-app.firebaseapp.com",
+    databaseURL: "https://halinhub-app-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "halinhub-app",
+    storageBucket: "halinhub-app.appspot.com",
+    messagingSenderId: "495601855540",
+    appId: "1:495601855540:web:3c0f369116799f5fd1ee61",
+    measurementId: "G-XD6Z33WV2P"
+};
+firebase.initializeApp(firebaseConfig);
